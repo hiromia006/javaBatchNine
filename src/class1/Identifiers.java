15a0eda04892da6380edc5fc44d50b7373d2fc15
Hiro Mia <hiromia006@gmail.com>
2024-05-15 21:50:30 +0600

    first commit

---
+++ src/class1/Identifiers.java
@@ -0,0 +1,12 @@
+package class1;
+
+public class Identifiers {
+    int number50=50;
+    int Number50=50;
+
+    public void printsomething(){
+
+    }
+}
+
+
---
+++ src/class1/Test.java
@@ -0,0 +1,10 @@
+package class1;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+    }
+}
