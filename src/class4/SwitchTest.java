3a48ec5d0de90eb1b662625ff83c2a08bd08ff30
Hiro Mia <hiromia006@gmail.com>
2024-05-18 22:14:45 +0600

    Submit Java 5th Assignment

---
+++ src/class4/SwitchTest.java
@@ -0,0 +1,22 @@
+package class4;
+
+public class SwitchTest {
+    public static void main(String[] args) {
+        int mark = 700;
+
+        switch (mark) {
+            case 80:
+                System.out.println("A++");
+                break;
+            case 70:
+                System.out.println("B++");
+                break;
+            case 60:
+                System.out.println("C++");
+                break;
+            default:
+                System.out.println("All Pass");
+
+        }
+    }
+}
